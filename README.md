dc2c449b5ccbd2cc60d06da4db1805a5b2ae7097
Damon Andrews <damonlovesufc@gmail.com>
2022-07-24 17:17:46 -0400

    Route Work

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Module13Project
+# E-commerce Back End Starter Code
+
+
+Here is a functional back-end for an e-commerce site. The started code has been modified, and there is a working Express.js API using Sequelize to interact with a MySQL database.
+
+GitHub Link:
+
+
+
+Video Demo Link:
+
---
+++ config/connection.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');  //this is good
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+
+//     // Create a connection object
+// const sequelize = new Sequelize(  //creates a new instance of sequelize class object// the const name is a "steve"
+// // Database name
+// 'library_db',
+// // User
+// 'root',
+// // Password
+// 'myPassword',
+// {
+//   // Database location  //options object telling where the db lives
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 3306
+// }
+// );
+module.exports = sequelize;
